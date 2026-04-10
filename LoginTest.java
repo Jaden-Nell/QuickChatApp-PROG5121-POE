cc9df8fc7ad1c6ac0761cd45cac4fb0fa6de4a7e
Jaden-Nell <jadennell.za@gmail.com>
2026-04-10 12:24:07 +0200

    Add basic test class for login func

--- LoginTest.java
+++ LoginTest.java
@@ -1,3 +1,35 @@
 public class LoginTest {
-    
+    public static void main(String[] args) {
+
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+
+        // Username test
+        if (login.checkUserName()) {
+            System.out.println("Username test passed");
+        } else {
+            System.out.println("Username test failed");
+        }
+
+        // Password test
+        if (login.checkPasswordComplexity()) {
+            System.out.println("Password test passed");
+        } else {
+            System.out.println("Password test failed");
+        }
+
+        // Phone test
+        if (login.checkCellPhoneNumber()) {
+            System.out.println("Phone test passed");
+        } else {
+            System.out.println("Phone test failed");
+        }
+
+        // Login test
+        boolean loginSuccess = login.loginUser("kyl_1", "Ch&sec@ke99!");
+        if (loginSuccess) {
+            System.out.println("Login test passed");
+        } else {
+            System.out.println("Login test failed");
+        }
+    }
 }

1daea8fd8ddfb4ba31e9c38566c5bfc5acf6cd9e
Jaden-Nell <jadennell.za@gmail.com>
2026-04-10 09:12:47 +0200

    Add login class with validation methods

--- Login.java
+++ Login.java
@@ -1,3 +1,75 @@
 public class Login {
-    
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String password;
+    private String cellPhoneNumber;
+
+    public Login(String firstName, String lastName, String userName, String password, String cellPhoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.password = password;
+        this.cellPhoneNumber = cellPhoneNumber;
+    }
+
+    public boolean checkUserName() {
+        return userName.contains("_") && userName.length() <= 5;
+    }
+
+    public boolean checkPasswordComplexity() {
+        boolean hasCapitalLetter = false;
+        boolean hasNumber = false;
+        boolean hasSpecialCharacter = false;
+
+        if (password.length() < 8) {
+            return false;
+        }
+
+        for (int i = 0; i < password.length(); i++) {
+            char ch = password.charAt(i);
+
+            if (Character.isUpperCase(ch)) {
+                hasCapitalLetter = true;
+            } else if (Character.isDigit(ch)) {
+                hasNumber = true;
+            } else if (!Character.isLetterOrDigit(ch)) {
+                hasSpecialCharacter = true;
+            }
+        }
+
+        return hasCapitalLetter && hasNumber && hasSpecialCharacter;
+    }
+
+    public boolean checkCellPhoneNumber() {
+        return cellPhoneNumber.matches("^\\+27\\d{9}$");
+    }
+
+    public String registerUser() {
+        if (!checkUserName()) {
+            return "Username is not correctly formatted; please ensure that your username contains an underscore and is no more than five characters in length.";
+        }
+
+        if (!checkPasswordComplexity()) {
+            return "Password is not correctly formatted; please ensure that the password contains at least eight characters, a capital letter, a number, and a special character.";
+        }
+
+        if (!checkCellPhoneNumber()) {
+            return "Cell phone number incorrectly formatted or does not contain international code.";
+        }
+
+        return "User has been registered successfully.";
+    }
+
+    public boolean loginUser(String enteredUserName, String enteredPassword) {
+        return userName.equals(enteredUserName) && password.equals(enteredPassword);
+    }
+
+    public String returnLoginStatus(boolean loginSuccess) {
+        if (loginSuccess) {
+            return "Welcome " + firstName + ", " + lastName + " it is great to see you again.";
+        } else {
+            return "Username or password incorrect, please try again.";
+        }
+    }
 }

34975c80f2bd57d58e393d4c1e4da756af3dae00
Jaden-Nell <jadennell.za@gmail.com>
2026-04-11 17:47:57 +0200

    Add JUnit tests and Maven functionality

--- src/test/java/LoginTest.java
+++ src/test/java/LoginTest.java
@@ -1,35 +1,53 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
 public class LoginTest {
-    public static void main(String[] args) {
 
+    @Test
+    public void testUsernameCorrect() {
         Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+        assertTrue(login.checkUserName());
+    }
 
-        // Username test
-        if (login.checkUserName()) {
-            System.out.println("Username test passed");
-        } else {
-            System.out.println("Username test failed");
-        }
+    @Test
+    public void testUsernameIncorrect() {
+        Login login = new Login("Kyle", "Smith", "kyle!!!!!!", "Ch&sec@ke99!", "+27838968976");
+        assertFalse(login.checkUserName());
+    }
 
-        // Password test
-        if (login.checkPasswordComplexity()) {
-            System.out.println("Password test passed");
-        } else {
-            System.out.println("Password test failed");
-        }
+    @Test
+    public void testPasswordCorrect() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+        assertTrue(login.checkPasswordComplexity());
+    }
 
-        // Phone test
-        if (login.checkCellPhoneNumber()) {
-            System.out.println("Phone test passed");
-        } else {
-            System.out.println("Phone test failed");
-        }
+    @Test
+    public void testPasswordIncorrect() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "password", "+27838968976");
+        assertFalse(login.checkPasswordComplexity());
+    }
 
-        // Login test
-        boolean loginSuccess = login.loginUser("kyl_1", "Ch&sec@ke99!");
-        if (loginSuccess) {
-            System.out.println("Login test passed");
-        } else {
-            System.out.println("Login test failed");
-        }
+    @Test
+    public void testCellPhoneCorrect() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+        assertTrue(login.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testCellPhoneIncorrect() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "08966553");
+        assertFalse(login.checkCellPhoneNumber());
+    }
+
+    @Test
+    public void testLoginSuccess() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+        assertTrue(login.loginUser("kyl_1", "Ch&sec@ke99!"));
+    }
+
+    @Test
+    public void testLoginFail() {
+        Login login = new Login("Kyle", "Smith", "kyl_1", "Ch&sec@ke99!", "+27838968976");
+        assertFalse(login.loginUser("wrong", "password"));
     }
 }
